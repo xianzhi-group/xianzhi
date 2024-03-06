41ab2ddc057d980066308539ff634648d4e5e7e5
Ethan Wang <max@xianzhi.io>
2024-03-06 13:29:52 +0800

    init properties

---
+++ xianzhi-system-server/xianzhi-system-bootstrap/src/main/java/io/xianzhi/system/SystemApplication.java
@@ -0,0 +1,18 @@
+package io.xianzhi.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统服务启动类<br>
+ *
+ * @author Ethan Wang
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+}
